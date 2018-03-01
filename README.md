d3134dca7720f8855ca62746b3a9a931b1b5541e
hengwu <youchuangqiba@163.com>
2018-03-01 18:58:54 +0800

    upd8

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # cnlp
+
+## Commands
+
+### virtualenv
+
+```bash
+$ pip install virtualenv
+$ virtualenv --no-site-packages venv
+$ source venv/Scripts/activate # 进入venv环境
+(venv) $ pip install -r requirements.txt # 安装依赖
+(venv) $ pip install scipy # 安装一个包
+(venv) $ pip freeze > requirements.txt # 保存安装包清单
+(venv) $ deactivate # 退出venv环境
+```
+
--- init_empty_folders.sh
+++ init_empty_folders.sh
@@ -2,14 +2,20 @@
 
 create_readme_in_empty_dir() {
     d=$1
-    echo "${d} is an empty folder"
+    echo "4: ${d} is an empty folder"
     touch "${d}/README.md"
 }
 
 for d in `ls`; do
+    echo "1: ${d}"
     if [ ! -d "${d}" ]; then
         continue
     fi
+    echo "2: ${d}"
+    if [ "${d}" == "venv" ]; then
+        continue
+    fi
+    echo "3: ${d}"
     find ${d} -type d -print0 | while IFS= read -r -d '' f; do
         [ "$(ls ${f})" ] || create_readme_in_empty_dir "${f}"
     done
